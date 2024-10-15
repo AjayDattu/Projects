c9db1cce541d7385e0a64358e06d0cd82971dd4c
Ajay Dattu <dattuajay005@gmail.com>
2024-10-15 21:41:48 +0530

    added code

--- CodeSum/src/App.jsx
+++ CodeSum/src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react';
+import {Center, ChakraProvider, Divider, Heading, VStack} from "@chakra-ui/react";
+import {githubLight} from '@uiw/codemirror-theme-github';
+import {python} from "@codemirror/lang-python";
+import CodeMirror from '@uiw/react-codemirror';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [text, setText] = useState("");
+    return (
+        <ChakraProvider>
+            <Center h={"100vh"}>
+                <VStack boxShadow={'md'} p={4} borderStyle={"solid"} 
+                 borderWidth={1} rounded={"lg"}>
+                    <Heading>Code Editor</Heading>
+                    <Divider/>
+                    <CodeMirror
+                        value={text}
+                        onChange={(newValue) => setText(newValue)}
+                        theme={githubLight}
+                        extensions={[python()]}
+                        basicSetup={{autocompletion: true}}
+                        minWidth={'500px'}
+                        minHeight={'500px'}
+                    />
+                </VStack>
+            </Center>
+        </ChakraProvider>
 
-  return (
-    <>
-     
-    </>
-  )
+    );
 }
 
-export default App
+export default App;
